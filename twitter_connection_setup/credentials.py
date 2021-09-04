8813d1550bd0b843f7f207bc6f5314c409290e4b
maximesnce <90032038+maximesnce@users.noreply.github.com>
2021-09-04 16:06:42 +0200

    Adding complementary files to the project

---
+++ twitter_connection_setup/credentials.py
@@ -0,0 +1,9 @@
+#Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'GXcToQDohAdhZXaQbuN1CU1YN'
+CONSUMER_SECRET = 'F3o2fvcYbveONh8vQ67WLHaSOxGyGvTHhPrFjj7xzjFn3Rp0WV'
+
+# Access:
+ACCESS_TOKEN  = '1194271340829773829-qPdFvQ75ux1asfHdLU2DIwRCrDjMev'
+ACCESS_SECRET = 'cd1f1v8a1PmVa0sOECVNlemBhA96GqfKbEeFUI4CcHwmJ'
